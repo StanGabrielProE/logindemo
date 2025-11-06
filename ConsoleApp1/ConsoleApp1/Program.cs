1665a8360bd2f9387dca96e27d4b073c0a55d3c9
StanGabrielProE <gabriel.stan@student.pxl.be>
2025-11-06 13:48:50 +0100

    DEMO-03 Inlogcontrole DEMO-04 Foutmelding

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace ConsoleApp1
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string input = Console.ReadLine();
+            Console.WriteLine("Invoer jouw username");
+            string username = Console.ReadLine();
+            GebruikersDataInvoeren(ref username, (user) => Regex.IsMatch(user, @"\w{8}"));
+            Console.WriteLine("Invoer jouw wachtword");
+            string userWachtword = Console.ReadLine();
         }
+
+        public static void GebruikersDataInvoeren ( ref string input, Predicate<string> conditie) 
+        {
+            int counter = 0;
+            while (!conditie(input)) 
+            {
+                Console.WriteLine($"{nameof(input)} niet juist is , jij hebt nog {3 - counter} keer ");
+                counter++;
+                    input = Console.ReadLine();
+                  
+                    
+                
+               if (counter == 3) 
+                {
+                    Console.WriteLine("Sorry maar je kan niet doorgaan");
+                    break;
+                }
+
+            };
+        }
+
     }
 }
--- ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7809f389eb334b3d9afb5b174ef86352c777e268")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17bc41012846f2ad70d99653792ebee015501ae9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
